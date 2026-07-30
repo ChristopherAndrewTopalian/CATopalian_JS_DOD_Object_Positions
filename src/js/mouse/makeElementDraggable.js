9f6d1d1e9266c41e6d70dc231e4917e6c83a5b45
Christopher Andrew Topalian <scriptingdata@gmail.com>
2026-07-30 12:35:49 -0400

    update

--- src/js/mouse/makeElementDraggable.js
+++ src/js/mouse/makeElementDraggable.js
@@ -57,63 +57,6 @@ function makeElementDraggable(element, worldEntry)
     element.addEventListener('mousedown', createMouseDownHandler(element, worldEntry, dragState));
 }
 
-// previous version below
-/*
-function createMouseDownHandler(element, state)
-{
-    return function(e)
-    {
-        if (e.target.tagName === 'TEXTAREA' || e.target.tagName === 'INPUT')
-        {
-            return;
-        }
-
-        e.preventDefault();
-
-        state.startX = e.clientX;
-        state.startY = e.clientY;
-
-        function mouseMoveHandler(e)
-        {
-            if (e.target.tagName === 'TEXTAREA' || e.target.tagName === 'INPUT')
-            {
-                return;
-            }
-
-            e.preventDefault();
-
-            let deltaX = state.startX - e.clientX;
-            let deltaY = state.startY - e.clientY;
-
-            state.startX = e.clientX;
-            state.startY = e.clientY;
-
-            let newTop = element.offsetTop - deltaY;
-            let newLeft = element.offsetLeft - deltaX;
-
-            element.style.top = newTop + "px";
-            element.style.left = newLeft + "px";
-        }
-
-        function mouseUpHandler()
-        {
-            document.removeEventListener('mousemove', mouseMoveHandler);
-            document.removeEventListener('mouseup', mouseUpHandler);
-        }
-
-        document.addEventListener('mousemove', mouseMoveHandler);
-        document.addEventListener('mouseup', mouseUpHandler);
-    };
-}
-
-function makeElementDraggable(element)
-{
-    let state = { startX: 0, startY: 0 };
-
-    element.addEventListener('mousedown', createMouseDownHandler(element, state));
-}
-*/
-
 //----//
 
 // Dedicated to God the Father
